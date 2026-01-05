11062b83b0b2e50fa17a6bc5515694e5cdf6f4ce
Zeke <ezekiel_alvarez@dlsu.edu.ph>
2026-01-05 19:00:49 +0800

    Add hello and test files

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "test.c"
+#include <string.h>
+
+damn();
+
+int main(){
+
+    int wo4w = damn(6);
+    printf("%d", wo4w);
+    return 0;
+
+    wow("lmfaooo");
+}
+
+int damn(int wow){
+    wow++;
+    return wow++;
+}
---
+++ test.c
@@ -0,0 +1,5 @@
+void wow(char lol[50]){
+    for (int i = 1; i <= strlen(lol); i++){
+        printf("lol\n");
+    }
+}
